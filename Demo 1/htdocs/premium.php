fdb2c0fb17c4e1a60b97caceb8a8e3c49ea8b8a9
agustintc <agustintc92@hotmail.com>
2016-06-03 01:32:20 -0300

    control premium

--- Demo 1/htdocs/premium.php
+++ Demo 1/htdocs/premium.php
@@ -34,24 +34,31 @@ $fecha_actual=date("Y-m-d");
 			</form> 
 			
 <?php
-	if(isset($_POST['enviar']))//para saber si el botón registrar fue presionado. 
-	{	 
+	$sql = 'SELECT email_usuario FROM pagos WHERE email_usuario="'.$_SESSION["session_username"].'"'; 
+        $mdb->set_charset('utf8');
+		$result=$mdb->query($sql);
+       
+				
+      if(mysqli_num_rows($result)==0 )
+		  {
+			if(isset($_POST['enviar']))//para saber si el botón registrar fue presionado. 
+			{	 
      
 		
-         $sql = "INSERT INTO pagos (id_pago, email_usuario, saldo_pago, fecha_pago) VALUES ('NULL', '".$_SESSION['session_username']."', '".$monto."', '".$fecha_actual."')"; 
-		 $mdb->set_charset('utf8');
-		 mysqli_query($mdb,$sql);
-		 $sql ="UPDATE usuarios SET tipo_usuario = '2' WHERE usuarios.email_usuario ='".$_SESSION['session_username']."'";
-		 mysqli_query($mdb,$sql);
-         ?>
-		 <div class="texto" >
-			<h2> <span> Usted es premium </span></h2>
-			</div>
-		 <?php
-		 
-		 
-	}    
-
+				$sql = "INSERT INTO pagos (id_pago, email_usuario, saldo_pago, fecha_pago) VALUES ('NULL', '".$_SESSION['session_username']."', '".$monto."', '".$fecha_actual."')"; 
+				$mdb->set_charset('utf8');
+				mysqli_query($mdb,$sql);
+				$sql ="UPDATE usuarios SET tipo_usuario = '2' WHERE usuarios.email_usuario ='".$_SESSION['session_username']."'";
+				mysqli_query($mdb,$sql);
+				?>
+				<div class="texto" >
+				<h2> <span> Usted es premium </span></h2>
+				</div>
+				<?php
+			}    
+		}
+		else{echo " Usted ya es premium";
+		}
 ?>
 
 

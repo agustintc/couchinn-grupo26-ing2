e41af5e08c8b5a9a16dff8c0c98370fd9bb6cbd2
Rodrigo Testa <testarodrigomaximiliano@gmail.com>
2016-06-15 18:09:55 -0300

    Base de datos

--- Base de datos/hospedajes.sql
+++ Base de datos/hospedajes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-06-2016 a las 23:08:42
+-- Tiempo de generación: 15-06-2016 a las 23:09:07
 -- Versión del servidor: 10.1.10-MariaDB
 -- Versión de PHP: 5.6.15
 
@@ -23,17 +23,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reservas`
+-- Estructura de tabla para la tabla `hospedajes`
 --
 
-CREATE TABLE `reservas` (
-  `id_reserva` int(11) NOT NULL,
+CREATE TABLE `hospedajes` (
+  `id_hospedaje` int(11) NOT NULL,
+  `nombre_hospedaje` text NOT NULL,
+  `descripcion_hospedaje` text NOT NULL,
+  `direccion_hospedaje` text NOT NULL,
+  `capacidad_hospedaje` text NOT NULL,
+  `nombre_tipo_hospedaje` text NOT NULL,
+  `id_usuario` text NOT NULL,
   `comienzo` date NOT NULL,
   `finalizacion` date NOT NULL,
-  `estado` int(11) NOT NULL,
-  `id_huesped` text NOT NULL,
-  `id_hospedaje` int(11) NOT NULL,
-  `comentario` text NOT NULL
+  `estado_hospedaje` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -41,20 +44,20 @@ CREATE TABLE `reservas` (
 --
 
 --
--- Indices de la tabla `reservas`
+-- Indices de la tabla `hospedajes`
 --
-ALTER TABLE `reservas`
-  ADD PRIMARY KEY (`id_reserva`);
+ALTER TABLE `hospedajes`
+  ADD PRIMARY KEY (`id_hospedaje`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `reservas`
+-- AUTO_INCREMENT de la tabla `hospedajes`
 --
-ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `hospedajes`
+  MODIFY `id_hospedaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

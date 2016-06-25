8c7a5e0be81759226f3ce778863ea8a7dfa8fae0
Rodrigo Testa <testarodrigomaximiliano@gmail.com>
2016-06-24 21:21:12 -0300

    Arreglo formato de fecha

--- Demo2-Prueba/chequear_reservas.php
+++ Demo2-Prueba/chequear_reservas.php
@@ -10,7 +10,7 @@
 		$result= $mdb->query($sql);
 	
 		while ($reservas = mysqli_fetch_assoc($result)){
-			if ($reservas['finalizacion'] <= date("Y/m/d")){
+			if ($reservas['finalizacion'] <= date("Y-m-d")){
 				$sql = "UPDATE reservas SET estado = 3 WHERE id_reserva = '" . $reservas['id_reserva'] . "'";
 				$mdb->query($sql);
 			}
